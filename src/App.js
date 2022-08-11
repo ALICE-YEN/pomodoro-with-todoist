a3a94af6d0a1049f2360c0bbc98f157aba54567b
Alice Yen <gp40424@gmail.com>
2022-08-11 23:59:34 +0800

    feat: ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { GiTomato } from 'react-icons/gi';
+import { IoMdSettings } from 'react-icons/io';
+import Task from './component/task';
+
 function App() {
   return (
-    <div className="flex justify-center bg-[#D95550]">
-      <div className="flex justify-between items-center min-w-[40%]">
-        <div className="text-white text-2xl font-bold p-2.5">番茄鐘</div>
-        <div className="text-white text-lg py-2 px-3">設定</div>
+    <>
+      <div className="flex justify-center bg-[#D95550] py-10">
+        <div className="flex justify-between items-center min-w-[40%] p-3.5 border-b">
+          <div className="flex items-center text-white text-2xl font-bold">
+            <GiTomato className="mr-2" />
+            番茄鐘
+          </div>
+          <div className="flex items-center text-white py-1.5 px-2 bg-[#FFFFFF33] rounded-md">
+            <IoMdSettings className="mr-1" />
+            設定
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="flex justify-center bg-[#D95550] py-10">
+        <Task />
+      </div>
+    </>
   );
 }
 
---
+++ src/component/task.js
@@ -0,0 +1,14 @@
+function Task() {
+  return (
+    <>
+      <div className="flex items-center">
+        <div className="w-5 h-5 rounded-full bg-white mr-3"></div>
+        <div className="py-5 w-[480px] text-white text-2xl font-bold border-b-2 border-[##ffffff33]">
+          Tasks
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Task;
