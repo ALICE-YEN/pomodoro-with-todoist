65b5920540ba3e74621ff0a62de60cb22c14ec60
Alice Yen <gp40424@gmail.com>
2022-08-12 01:41:31 +0800

    feat: ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { GiTomato } from 'react-icons/gi';
 import { IoMdSettings } from 'react-icons/io';
+import { IoMdAddCircle } from 'react-icons/io';
+
 import Task from './component/task';
 
 function App() {
   return (
     <>
       <div className="flex justify-center bg-[#D95550] py-10">
-        <div className="flex justify-between items-center min-w-[40%] p-3.5 border-b">
-          <div className="flex items-center text-white text-2xl font-bold">
+        <div className="flex justify-between items-center min-w-[40%] p-3.5 border-b text-white">
+          <div className="flex items-center text-2xl font-bold">
             <GiTomato className="mr-2" />
             番茄鐘
           </div>
-          <div className="flex items-center text-white py-1.5 px-2 bg-[#FFFFFF33] rounded-md">
+          <div className="flex items-center py-1.5 px-2 bg-[#FFFFFF33] rounded-md">
             <IoMdSettings className="mr-1" />
             設定
           </div>
@@ -19,6 +21,12 @@ function App() {
       </div>
       <div className="flex justify-center bg-[#D95550] py-10">
         <Task />
+      </div>
+      <div className="flex justify-center bg-[#D95550] py-10">
+        <div className="flex items-center justify-center w-[480px] bg-[#0000001A] text-white font-bold border-dashed rounded-md border-2 border-white opacity-70 py-3.5">
+          <IoMdAddCircle className="mr-2" />
+          Add Task
+        </div>
       </div>
     </>
   );
--- src/component/task.js
+++ src/component/task.js
@@ -1,10 +1,19 @@
+import { BsFillTrashFill } from 'react-icons/bs';
+import { FiEdit } from 'react-icons/fi';
+
 function Task() {
   return (
     <>
       <div className="flex items-center">
-        <div className="w-5 h-5 rounded-full bg-white mr-3"></div>
-        <div className="py-5 w-[480px] text-white text-2xl font-bold border-b-2 border-[##ffffff33]">
-          Tasks
+        <div className="flex justify-between py-5 w-[480px] border-b-2 border-[##ffffff33] text-white px-1.5">
+          <div className="flex items-center">
+            <div className="w-5 h-5 bg-white mr-5"></div>
+            <div className="text-xl font-bold">Tasks</div>
+          </div>
+          <div className="flex items-center">
+            <FiEdit size={20} className="mr-4" />
+            <BsFillTrashFill size={20} />
+          </div>
         </div>
       </div>
     </>
