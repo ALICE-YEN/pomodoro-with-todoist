7eac76708bd0ec7ba96c2a95f5ef5013dbbb45a5
Alice Yen <aliceyen@xarefit.com>
2022-08-11 17:59:15 +0800

    feat: setup tailwind+ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div></div>
+  return (
+    <div className="flex justify-center bg-[#D95550]">
+      <div className="flex justify-between items-center min-w-[40%]">
+        <div className="text-white text-2xl font-bold p-2.5">番茄鐘</div>
+        <div className="text-white text-lg py-2 px-3">設定</div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
